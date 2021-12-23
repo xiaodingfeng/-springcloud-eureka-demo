c79392153106317c89d9ccbe8234d29abb76311a
肖定峰 <1440651649@qq.com>
2021-12-23 15:54:15 +0800

    commit code

--- eureka-service-consume/src/main/java/com/xiaobai/eurekaserviceconsume/TestController.java
+++ eureka-service-consume/src/main/java/com/xiaobai/eurekaserviceconsume/TestController.java
@@ -1,5 +1,7 @@
 package com.xiaobai.eurekaserviceconsume;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,11 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class TestController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(TestController.class);
     @Autowired
     private CartFeignClient client;
 
     @GetMapping("/cart")
     public String addCart() {
+        LOGGER.info("被调用了");
         return client.addCart();
     }
 }
--- eureka-service-provider/src/main/java/com/xiaobai/eurekaserviceprovider/CartController.java
+++ eureka-service-provider/src/main/java/com/xiaobai/eurekaserviceprovider/CartController.java
@@ -24,6 +24,7 @@ public class CartController {
 
     @PostMapping("/cart")
     public String addCart() {
+        LOGGER.info("被调用了");
         return "hello world!";
     }
 
