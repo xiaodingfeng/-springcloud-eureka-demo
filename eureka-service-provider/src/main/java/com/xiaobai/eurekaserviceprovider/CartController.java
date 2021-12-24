d03d77525f16aa4906d359e43b50d8b8f70470ce
肖定峰 <1440651649@qq.com>
2021-12-24 11:18:36 +0800

    provider增加mq

--- eureka-service-provider/src/main/java/com/xiaobai/eurekaserviceprovider/CartController.java
+++ eureka-service-provider/src/main/java/com/xiaobai/eurekaserviceprovider/CartController.java
@@ -1,13 +1,20 @@
 package com.xiaobai.eurekaserviceprovider;
 
+import com.rabbitmq.client.Channel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 /**
  * @author dingfeng.xiao
@@ -22,12 +29,27 @@ public class CartController {
     @Value("${spring.application.name}")
     private String value;
 
+    @Autowired
+    RabbitTemplate rabbitTemplate;
+
+
     @PostMapping("/cart")
     public String addCart() {
+
+        ConcurrentMap<Integer,Object> myMap = new ConcurrentHashMap<>(4);
+        myMap.put(0,"神司马懿");
+        myMap.put(1,"界徐盛");
+        myMap.put(2,"曹钝");
+        myMap.put(3,"骆统");
+        rabbitTemplate.convertAndSend("amq.direct","2",myMap);
         LOGGER.info("被调用了");
         return "hello world!";
     }
 
+    @RabbitListener(queues = "swl.direct")
+    public void listen(ConcurrentMap<Integer,Object> msg, Message message, Channel channel){
+        LOGGER.info("mq pull {}" , msg);
+    }
 
     @RequestMapping("/hello")
     public String hello() {
--- eureka-service-provider/src/main/java/com/xiaobai/eurekaserviceprovider/EurekaServiceProviderApplication.java
+++ eureka-service-provider/src/main/java/com/xiaobai/eurekaserviceprovider/EurekaServiceProviderApplication.java
@@ -1,8 +1,11 @@
 package com.xiaobai.eurekaserviceprovider;
 
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 /**
  * @author dingfeng.xiao
@@ -10,6 +13,11 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @EnableDiscoveryClient
 public class EurekaServiceProviderApplication {
+
+    @Bean
+    public MessageConverter getMessageConverter(){
+        return new Jackson2JsonMessageConverter();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(EurekaServiceProviderApplication.class, args);
